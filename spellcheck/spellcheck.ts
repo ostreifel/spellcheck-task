23252754c4d00487c64e864102f0c8b4658a37e8
Otto Streifel <ottost@microsoft.com>
2017-08-25 13:00:06 -0700

    import glob

--- spellcheck/spellcheck.ts
+++ spellcheck/spellcheck.ts
@@ -1,15 +1,19 @@
+import * as fs from "fs";
+import * as glob from "glob";
 import tl = require("vsts-task-lib/task");
+
 // npm install vsts-task-lib
 
 // get task parameters
-const variable1: string = tl.getPathInput("files", false, true);
+const fileGlob: string = tl.getPathInput("files", false, true);
 const variable2: string = tl.getInput("variable2", true);
 
 async function run(): Promise<void> {
     try {
         // do your actions
-        tl.debug("variable1:" + variable1);
+        tl.debug("fileGlob:" + fileGlob);
         tl.debug("variable2:" + variable2);
+        // glob.
 
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
