f233a7503d7585dea411b7d87c5a67fa921f1470
Otto Streifel <ottost@microsoft.com>
2017-08-25 11:39:01 -0700

    tslint

--- spellcheck/spellcheck.ts
+++ spellcheck/spellcheck.ts
@@ -2,16 +2,14 @@ import tl = require("vsts-task-lib/task");
 // npm install vsts-task-lib
 
 // get task parameters
-let variable1: string = tl.getPathInput("variable1", false, true);
-let variable2: string = tl.getInput("variable2", true);
-
-
+const variable1: string = tl.getPathInput("files", false, true);
+const variable2: string = tl.getInput("variable2", true);
 
 async function run(): Promise<void> {
     try {
         // do your actions
-        tl.debug("variable1:" +variable1);
-        tl.debug("variable2:" +variable2);
+        tl.debug("variable1:" + variable1);
+        tl.debug("variable2:" + variable2);
 
     } catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
