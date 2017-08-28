a64b8022612f5c912e79d33eda77a8a036b0109e
Otto Streifel <ottost@microsoft.com>
2017-08-28 09:33:54 -0700

    stubs

--- spellcheck/spellcheck.ts
+++ spellcheck/spellcheck.ts
@@ -1,7 +1,37 @@
-import * as fs from "fs";
 import * as glob from "glob";
-import * as path from "path";
+import * as Q from "q";
 import tl = require("vsts-task-lib/task");
+
+interface IFileErrors {
+    readonly filePath: string;
+    readonly misspellings: IMisspelling[];
+}
+interface IMisspelling {
+    readonly line: string;
+    readonly column: string;
+    readonly text: string;
+}
+
+async function checkFile(filePath: string): Promise<IFileErrors> {
+    return {filePath, misspellings: []};
+}
+
+async function processErrors(errors: IFileErrors[]): Promise<void> {
+    let failed = tl.TaskResult.Succeeded;
+    let errorCount = 0;
+    for (const {filePath, misspellings} of errors) {
+        if (misspellings.length === 0) {
+            continue;
+        }
+        failed = tl.TaskResult.Failed;
+        errorCount += misspellings.length;
+        tl.error(`Misspellings in ${filePath}`);
+        for (const {line, column, text} of misspellings) {
+            tl.error(`${line}:${column} Misspelling '${text}'`);
+        }
+    }
+    tl.setResult(failed, `${errorCount} misspellings detected`);
+}
 
 async function run(): Promise<void> {
     try {
@@ -13,7 +43,9 @@ async function run(): Promise<void> {
         console.log("fileGlob:" + fileGlob);
         console.log("includeRegexString:" + includeRegexString);
         tl.setResult(tl.TaskResult.Succeeded, "No spelling errors");
+        const files = glob.sync(fileGlob);
 
+        Q.all(files.map((f) => checkFile(f))).then(processErrors);
     } catch (err) {
         console.log("err", err);
         tl.setResult(tl.TaskResult.Failed, err.message);
